cd807c374f77ba55e60ff41c0fed067ba3a58c83
MrZoraman <MrZoraman@gmail.com>
2021-04-07 12:29:38 -0700

    Add model

---
+++ Model/Formula.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ToyMvvm.Model
+{
+    public class Formula
+    {
+        private readonly double _a;
+        private readonly double _b;
+        private readonly double _c;
+
+        public Formula(double a, double b, double c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public double Calculate(double x)
+        {
+            return Math.Pow(_a, 2) * x + _b * x + _c;
+        }
+    }
+}
