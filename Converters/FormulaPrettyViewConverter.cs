bc4f48a0b3cc377000d1462ee239f703789f3bec
MrZoraman <MrZoraman@gmail.com>
2021-04-12 05:16:08 -0700

    Use converter for formula view

---
+++ Converters/FormulaPrettyViewConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using ToyMvvm.Model;
+
+namespace ToyMvvm.Converters
+{
+    public class FormulaPrettyViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Formula formula)
+            {
+                return $"{formula.A}X^2 + {formula.B}X + {formula.C}";
+            }
+
+            return "???";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- DataContext/FormulaContext.cs
+++ DataContext/FormulaContext.cs
@@ -1,8 +1,9 @@
-﻿using ToyMvvm.Model;
+﻿using System.ComponentModel;
+using ToyMvvm.Model;
 
 namespace ToyMvvm.DataContext
 {
-    public class FormulaContext
+    public class FormulaContext : INotifyPropertyChanged
     {
         private readonly Formula _formula;
 
@@ -11,6 +12,11 @@ namespace ToyMvvm.DataContext
             _formula = formula;
         }
 
+        public Formula Formula => _formula;
+
         public string PrettyView => $"{_formula.A}X^2 + {_formula.B}X + {_formula.C}";
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
